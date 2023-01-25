d3b0ce5a59504bc936855a4fbfe63da45dc1cfd2
Jihwan Park <jipark9618@gmail.com>
2023-01-24 22:23:02 -0500

    DELETE NOT WORKING...

---
+++ out/server.js
@@ -0,0 +1,173 @@
+import express from "express";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import * as url from "url";
+let app = express();
+app.use(express.json());
+// create database "connection"
+// use absolute path to avoid this issue
+// https://github.com/TryGhost/node-sqlite3/issues/441
+let __dirname = url.fileURLToPath(new URL("..", import.meta.url));
+let dbfile = `${__dirname}database.db`;
+let db = await open({
+    filename: dbfile,
+    driver: sqlite3.Database,
+});
+await db.get("PRAGMA foreign_keys = ON");
+//GET Request to fetch all the books 
+app.get("/books/showall", async (req, res) => {
+    try {
+        let books = await db.all("SELECT * FROM books");
+        return res.json(books);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//GET Request to fetch all the authors 
+app.get("/authors/showall", async (req, res) => {
+    try {
+        let authors = await db.all("SELECT * FROM authors");
+        return res.json(authors);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//DELETE Request to delete a book from the database given its id
+app.delete("/books/:id", async (req, res) => {
+    try {
+        let bId = req.params.id;
+        await db.run(`DELETE * FROM books WHERE id = ${bId}`);
+        return res.status(202).json();
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//DELETE Request to delete an author from the database given his or her id 
+app.delete("/authors/:id", async (req, res) => {
+    try {
+        let aId = req.params.id;
+        await db.run(`DELETE * FROM authors WHERE id = ${aId}`);
+        return res.status(202).json();
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//GET Request to fetch a single book by its id 
+app.get("/books/:id", async (req, res) => {
+    try {
+        let bookId = req.params.id;
+        let book = await db.all(`SELECT * FROM books WHERE id = ${bookId}`);
+        return res.json(book);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//GET Request to fetch a single author by his or her id
+app.get("/authors/:id", async (req, res) => {
+    try {
+        let authorId = req.params.id;
+        let author = await db.all(`SELECT * FROM authors WHERE id = ${authorId}`);
+        return res.json(author);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//GET Request to fetch all the books published in a specific year  
+app.get("/books", async (req, res) => {
+    if (!req.query.pub_year) {
+        return res.status(400).json({ error: "Published Year is required" });
+    }
+    try {
+        let pub_year = req.query.pub_year;
+        let filtered = await db.all(`SELECT * FROM books WHERE pub_year = ${pub_year}`);
+        return res.json(filtered);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//GET Request to fetch all the authors with a given name 
+app.get("/authors", async (req, res) => {
+    if (!req.query.name) {
+        return res.status(400).json({ error: "name is required" });
+    }
+    try {
+        let name = req.query.name;
+        let filtered = await db.all(`SELECT * FROM authors WHERE name = "${name}"`);
+        return res.json(filtered);
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+//POST Request to insert a new book into a database
+//All the fields are required
+app.post("/books", async (req, res) => {
+    if (!req.body.id) {
+        return res.status(400).json({ error: "id is required" });
+    }
+    if (!req.body.author_id) {
+        return res.status(400).json({ error: "author_id is required" });
+    }
+    if (!req.body.title) {
+        return res.status(400).json({ error: "title is required" });
+    }
+    if (!req.body.pub_year) {
+        return res.status(400).json({ error: "pub_year is required" });
+    }
+    if (!req.body.genre) {
+        return res.status(400).json({ error: "genre is required" });
+    }
+    let bookId = req.body.id;
+    let bookAuthorId = req.body.author_id;
+    let bookTitle = req.body.title;
+    let bookPubYear = req.body.pub_year;
+    let bookGenre = req.body.genre;
+    try {
+        let sql = await db.prepare("INSERT INTO books VALUES (?, ?, ?, ?, ?)");
+        await sql.bind([bookId, bookAuthorId, bookTitle, bookPubYear, bookGenre]);
+        await sql.run();
+        return res.json({ message: "Post Success" });
+    }
+    catch (err) {
+        return res.status(400).json();
+    }
+});
+//POST Request to insert a new author into a database
+//All the fields are required
+app.post("/authors", async (req, res) => {
+    if (!req.body.id) {
+        return res.status(400).json({ error: "id is required" });
+    }
+    if (!req.body.name) {
+        return res.status(400).json({ error: "name is required" });
+    }
+    if (!req.body.bio) {
+        return res.status(400).json({ error: "bio is required" });
+    }
+    let authorId = req.body.id;
+    let authorName = req.body.name;
+    let authorBio = req.body.bio;
+    try {
+        let sql = await db.prepare("INSERT INTO authors (id, name, bio) VALUES (?, ?, ?)");
+        await sql.bind([authorId, authorName, authorBio]);
+        await sql.run();
+        return res.json({ message: "Post Success" });
+    }
+    catch (err) {
+        return res.status(400).json({ error: `${err}` });
+    }
+});
+// run server
+let port = 3000;
+let host = "localhost";
+let protocol = "http";
+app.listen(port, host, () => {
+    console.log(`${protocol}://${host}:${port}`);
+});
---
+++ src/server.test.ts
@@ -0,0 +1,98 @@
+import axios, { AxiosError } from "axios";
+
+let port = 3000;
+let host = "localhost";
+let protocol = "http";
+let baseUrl = `${protocol}://${host}:${port}`;
+
+//POST Request to insert an author into a database
+//All the fields are required
+//Return a success message if no error occurs
+test("POST /authors returns a success message", async () => {
+
+    let id = "1";
+    let name = "Tolstoy";
+    let bio = "Russian Writier";
+
+    let result = await axios.post(`${baseUrl}/authors`, { id, name, bio });
+    expect(result.data).toEqual({ message: "Post Success" });
+})
+
+//POST Request to insert a book into a database
+//All the fields are required
+//Return a success message if no error occurs
+test("POST /books returns a success message", async () => {
+    let id = "1";
+    let author_id = "1";
+    let title = "Resurrection";
+    let pub_year = "2012";
+    let genre = "fiction";
+
+    let result = await axios.post(`${baseUrl}/books`, { id, author_id, title, pub_year, genre });
+    expect(result.data).toEqual({ message: "Post Success" });
+})
+
+
+
+//GET Request to fetch a single book by its id
+//Return 200 status code if no error occurs
+test("Check GET /books/:id returns 200 status code", async () => {
+    let id = "1";
+    let response = await axios.get(`${baseUrl}/books/${id}`);
+    expect(response.status).toEqual(200);
+})
+
+//GET Request to fetch a single author by his or her id
+//Return 200 status code if no error occurs
+test("Check GET /authors/:id returns 200 status code", async () => {
+    let id = "1";
+    let response = await axios.get(`${baseUrl}/authors/${id}`);
+    expect(response.status).toEqual(200);
+})
+
+//GET Request to fetch all the books published in a given year
+//Return 200 status code if no error occurs
+test("Check GET /books returns 200 status code", async () => {
+    let pub_year = "2012";
+    let response = await axios.get(`${baseUrl}/books?pub_year=${pub_year}`);
+    expect(response.status).toEqual(200);
+})
+
+//GET Request to fetch all the authors with a given name 
+//Return 200 status code if no error occurs
+test("Check GET /authors returns 200 status code", async () => {
+    let name = "Tolstoy";
+    let response = await axios.get(`${baseUrl}/authors?name=${name}`);
+    expect(response.status).toEqual(200);
+})
+
+//GET Requesst to fetch all the books
+//Return 200 status code if no error occurs
+test("GET /books/showall returns 200 status code", async () => {
+    let response = await axios.get(`${baseUrl}/books/showall`);
+    console.log(response);
+    expect(response.status).toEqual(200);
+})
+
+//GET Request to fetch all the authors
+//Return 200 status code if no error occurs
+test("GET /authors/showall returns 200 status code", async () => {
+    let response = await axios.get(`${baseUrl}/authors/showall`);
+    expect(response.status).toEqual(200);
+})
+
+//DELETE Request to delete a book given its id
+//Return 202 status code if accepted
+test("DELETE /books/:id returns 202 status code", async () => {
+    let id = 1;
+    let response = await axios.delete(`${baseUrl}/books/${id}`);
+    expect(response.status).toEqual(202);
+})
+
+//DELETE Request to delete an author given his or her 
+//Return 202 status code if accepted
+test("DELETE /authors/:id returns 202 status code", async () => {
+    let id = 1;
+    let response = await axios.delete(`${baseUrl}/authors/${id}`);
+    expect(response.status).toEqual(202);
+})
